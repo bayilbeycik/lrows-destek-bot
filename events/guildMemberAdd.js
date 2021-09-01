48e2c257c04b6485757324c2ebbec1136a13450b
bayilbeycik <89037567+bayilbeycik@users.noreply.github.com>
2021-09-02 01:39:29 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,7 +8,6 @@ const message = require('./message');
 
     let username = member.user.username;
     //
-    member.send('Merhaba ***'+ username +'*** Öncelikle Hoşgeldin. **Gökkuşağı Bilişim**\'i Seçtiğin İçin Teşekkür Ederiz! Eğer Bir Sorun Olursa <#834490346062610475> Kanalından Destek Açmanı Öneriyoruz!  Eğer Bir Öneriniz Olursa <#835305565176856646> Kanalından Önerinizi Yapabilirsini! **İyi Günler**! ');
     
 
 };
